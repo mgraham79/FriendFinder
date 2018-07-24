e163965d6bcde5c913a47e955db8fe0acf8eb472
Entyce <38805393+mgraham79@users.noreply.github.com>
2018-07-23 20:42:41 -0700

    Updated code for all files

---
+++ app/data/friends.js
@@ -0,0 +1,70 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+    {
+      name: 'Margot Robbie',
+      profilePic: 'https://www.mvff.com/wp-content/uploads/2017/10/SS-INTLSS4-0412-MR_sm.jpg',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    },
+    {
+      name: 'Dwayne Johnson',
+      profilePic: 'http://www.gstatic.com/tv/thumb/persons/235135/235135_v9_ba.jpg',
+      scores: [
+        "3", 
+        "2", 
+        "4", 
+        "5",
+        "4",
+        "3", 
+        "2", 
+        "4", 
+        "5",
+        "4"
+      ]
+    },
+    {
+      name: 'Kevin Hart',
+      profilePic: 'http://www.celebritycarsblog.com/wp-content/uploads/Kevin-Hart1-e1436755187269.png',
+      scores: [
+        "1", 
+        "5", 
+        "4", 
+        "3",
+        "1",
+        "4", 
+        "5", 
+        "2", 
+        "1",
+        "5"
+      ]
+    },
+    {
+      name: 'Rihanna',
+      profilePic: 'http://www.rihannanow.com/wp-content/uploads/2018/04/March27-FB_SU18_BEAUTY_RGB_2000X2000_72DPI_17-300x300.jpg',
+      scores: [
+        "5", 
+        "5", 
+        "5", 
+        "5",
+        "5",
+        "5", 
+        "5", 
+        "5", 
+        "5",
+        "5"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
